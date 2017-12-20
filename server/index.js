2be8501c74f9e7d5512640169fc66ecdbd9de21f
timknapp12 <timknapp12@gmail.com>
2017-12-20 10:05:47 -0700

    added hosting

--- server/index.js
+++ server/index.js
@@ -80,7 +80,7 @@ app.get("/auth", passport.authenticate("auth0"));
 app.get(
   "/auth/callback",
   passport.authenticate("auth0", {
-    successRedirect: "/home",
+    successRedirect: "http://timpeacockpottery.com/#/home",
     failureRedirect: "/auth"
   })
 );
